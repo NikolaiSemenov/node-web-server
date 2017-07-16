4d86b64ed4f7b949b9c8ebb69e2934c6f023fe99
Nikolai Semenov <nickola105s@gmail.com>
2017-07-16 15:14:52 +0700

    Initial commit

---
+++ server.js
@@ -0,0 +1,34 @@
+const express = require('express');
+const hbs = require('hbs');
+
+var app = express();
+
+hbs.registerPartials(__dirname + '/views/partials')
+app.set('view engine', 'hbs');
+
+// app.use((req, res, next) => {
+// 	res.render('maintenance.hbs');
+// });
+
+app.use(express.static(__dirname + '/public'));
+
+hbs.registerHelper('getCurrentYear', () => {
+	return new Date().getFullYear();
+});
+
+app.get('/', (req, res) => {
+	res.render('home.hbs', {
+		pageTitle: 'Home Page',
+		welcomeMessage: 'Welocome my friends'
+	})
+});
+
+app.get('/about', (req, res) => {
+	res.render('about.hbs', {
+		pageTitle: 'About Page'
+	});
+});
+
+app.listen(3000, () => {
+	console.log('Server is up on port 3000');
+});
